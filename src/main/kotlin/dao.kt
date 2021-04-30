87ec6bb47754d649bb3b8cb2be1988d96b7ba986
Peter Klimai <pklimai@gmail.com>
2021-04-30 23:28:10 +0300

    WIP

--- src/main/kotlin/dao.kt
+++ src/main/kotlin/dao.kt
@@ -1,18 +1,16 @@
 package com.example
 
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.Closeable
 
 interface DAOEventInterface : Closeable {
     fun init()
     fun createEvent(file_ptr: Int, event_num: Int, period: Int, run: Int, sw_ver: Short, all_tracks: Int)
+
 //    fun updateEvent(file_ptr: Int, event_num: Int, period: Int, run: Int, sw_ver: Short, all_tracks: Int)
 //    fun deleteEvent(file_ptr: Int, event_num: Int)
-//    fun getEvent(file_ptr: Int, event_num: Int): Event?
+    fun getEvent(file_ptr: Int, event_num: Int): Event?
     fun getAllEvents(): List<Event>
 }
 
@@ -34,16 +32,25 @@ class EventDAO(val db: Database) : DAOEventInterface {
             Unit
         }
 
-//    override fun updateEvent(file_ptr: Int, event_num: Int, period: Int, run: Int, sw_ver: Short, all_tracks: Int) {
+    //    override fun updateEvent(file_ptr: Int, event_num: Int, period: Int, run: Int, sw_ver: Short, all_tracks: Int) {
 //        TODO("Not yet implemented")
 //    }
 //    override fun deleteEvent(file_ptr: Int, event_num: Int) {
 //        TODO("Not yet implemented")
 //    }
 //
-//    override fun getEvent(file_ptr: Int, event_num: Int): Event? {
-//        TODO("Not yet implemented")
-//    }
+    override fun getEvent(file_ptr: Int, event_num: Int): Event? = transaction(db) {
+        Events.select { (Events.file_ptr eq file_ptr) and (Events.event_num eq event_num) }.map {
+            Event(
+                it[Events.file_ptr],
+                it[Events.event_num],
+                it[Events.period],
+                it[Events.run],
+                it[Events.sw_ver].toShort(),
+                it[Events.all_tracks]
+            )
+        }.singleOrNull()
+    }
 
     override fun getAllEvents(): List<Event> =
         transaction(db) {
