e7be63cd3c9864979be8b70b6eb60279aedc371b
Peter Klimai <pklimai@gmail.com>
2021-06-27 14:55:08 +0300

    Drop Exposed

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,7 +9,6 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.html.*
-import org.jetbrains.exposed.sql.Database
 import java.sql.DriverManager
 
 val URL = "jdbc:postgresql://192.168.65.52:5000/event_db"
@@ -40,12 +39,7 @@ fun Application.main() {
     install(ContentNegotiation) {
         jackson {}
     }
-    val dao = EventDAO(
-        Database.connect(URL, driver = DRIVER, user = USER, password = PASS)
-    )
     val conn = DriverManager.getConnection(URL, USER, PASS)
-
-    // println(dao.getAllEvents())
 
     // println("Working Directory = ${System.getProperty("user.dir")}")
 
@@ -67,8 +61,6 @@ fun Application.main() {
                     p { +"CPUs: ${runtime.availableProcessors()}. Memory free/total/max: ${runtime.freeMemory()} / ${runtime.totalMemory()} / ${runtime.maxMemory()}." }
                     hr {}
                     h3 { +"REST API" }
-                    p { a(href = "/event_api/v1/exposed/all_events") { +"Exposed - All events" } }
-                    p { a(href = "/event_api/v1/exposed/all_joined") { +"Exposed - All events joined" } }
                     p { a(href = "/event_api/v1/raw/joined") { +"Raw - All events joined" } }
                     hr {}
                     h3 { +"WebUI" }
@@ -290,102 +282,6 @@ fun Application.main() {
 
             }
         }
-
-
-        route("/event_api/v1/exposed") {
-
-            route("/all_events") {
-                get() {
-                    call.respond(mapOf("events" to dao.getAllEvents()))
-                }
-            }
-
-            route("/all_joined") {
-                get() {
-                    call.respond(mapOf("events_joined" to dao.getAllEventsJoined()))
-                }
-            }
-
-            // Example URL -- http://127.0.0.1:8080/events/4/10
-            get("/{file_ptr}/{event_num}") {
-                val file_ptr = call.parameters["file_ptr"]?.toInt()
-                val event_num = call.parameters["event_num"]?.toInt()
-                if (file_ptr != null && event_num != null) {
-                    val resp = dao.getEvent(file_guid = file_ptr, event_num = event_num)
-                    if (resp != null) {
-                        call.respond(resp)
-                    } else {
-                        call.respond("No such event found!")
-                    }
-                }
-            }
-
-            // Example URL -- http://127.0.0.1:8080/events/getevent?file_ptr=4&event_num=10
-            get("/getevent") {
-                val file_ptr = call.parameters["file_ptr"]?.toInt()
-                val event_num = call.parameters["event_num"]?.toInt()
-                if (file_ptr != null && event_num != null) {
-                    val resp = dao.getEvent(file_guid = file_ptr, event_num = event_num)
-                    if (resp != null) {
-                        call.respond(resp)
-                    } else {
-                        call.respond("No such event found!")
-                    }
-                } else {
-                    call.respond("file_ptr and event_num are required here")
-                }
-            }
-
-            // Example URL -- http://127.0.0.1:8080/events/search?period=7&run=5000
-            get("/search") {
-                val period = call.parameters["period"]?.toInt()
-                val run = call.parameters["run"]?.toInt()
-                if (period != null && run != null) {
-                    call.respond(mapOf("events" to dao.searchEvents(period, run)))
-                }
-            }
-
-            // **NB**: Content-Type is mandatory
-            // POST http://127.0.0.1:8080/events/create
-            // Content-Type: application/json
-            //
-            // { "file_ptr": 10, "event_num": 22, "period": 7, "run": 5000, "software_id": 1, "all_tracks": 55 }
-            post("/create") {
-                val event = call.receive<Event>()
-                dao.createEvent(
-                    event.file_guid,
-                    event.event_number,
-                    event.period_number,
-                    event.run_number.toInt(),
-                    event.software_id.toInt(),
-                    event.track_number
-                )
-                call.respond("Event created!")
-            }
-
-            /*
-             { "events": [
-               { "file_ptr": 10, "event_num": 31, "period": 7, "run": 5000, "software_idr": 1, "all_tracks": 55 },
-               { "file_ptr": 10, "event_num": 32, "period": 7, "run": 5000, "software_id": 1, "all_tracks": 55 }
-             ] }
-            */
-            post("/create-multiple") {
-                val events = call.receive<EventList>()
-                events.events.forEach { event ->
-                    dao.createEvent(
-                        event.file_guid,
-                        event.event_number,
-                        event.period_number,
-                        event.run_number.toInt(),
-                        event.software_id.toInt(),
-                        event.track_number
-                    )
-                }
-                call.respond("${events.events.size} events created!")
-            }
-
-        }
-
 
     }
 }
--- src/main/kotlin/models.kt
+++ src/main/kotlin/models.kt
@@ -1,38 +1,20 @@
 package com.example
 
-import org.jetbrains.exposed.sql.Table
-
 data class Software(
     val software_id: Short,
     val software_version: String
 )
-
-object Softwares : Table("software_") {
-    val software_id = integer("software_id").primaryKey()
-    val software_version = varchar("software_version", 20) // .uniqueIndex()
-}
 
 data class Storage(
     val storage_id: Short,
     val storage_name: String
 )
 
-object Storages : Table("storage_") {
-    val storage_id = integer("storage_id").primaryKey()
-    val storage_name = varchar("storage_name", 20)
-}
-
 data class File(
     val file_guid: Int,
     val storage_id: Short,
     val file_path: String
 )
-
-object Files : Table("file_") {
-    val file_guid = integer("file_guid").primaryKey()
-    val storage_id = integer("storage_id").references(Storages.storage_id)
-    val file_path = varchar("file_path", 255)
-}
 
 data class Event(
     val file_guid: Int,
@@ -42,24 +24,6 @@ data class Event(
     val run_number: Short,
     val track_number: Int
 )
-
-data class EventJoined(
-    val file_guid: Int,
-    val event_number: Int,
-    val software_version: String,
-    val period_number: Short,
-    val run_number: Short,
-    val track_number: Int
-)
-
-object Events : Table("bmn_event") {
-    val file_guid = integer("file_guid").primaryKey().references(Files.file_guid)
-    val event_number = integer("event_number").primaryKey()
-    val software_id = integer("software_id").references(Softwares.software_id)         // there is no 'short'...
-    val period_number = integer("period_number")
-    val run_number = integer("run_number")
-    val track_number = integer("track_number").default(-1)
-}
 
 data class EventList(val events: List<Event>)
 
