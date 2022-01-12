dbf427f5523f36de58318e1660852d3aa6eac03e
Peter Klimai <pklimai@gmail.com>
2022-01-12 15:05:13 +0300

    Upd table

--- src/jsMain/kotlin/EMDTable.kt
+++ src/jsMain/kotlin/EMDTable.kt
@@ -11,29 +11,47 @@ external interface EMDTableProps : Props {
 
 val EMDTable = fc<EMDTableProps> { props ->
     div("lightgreen") {
-        h4 {
-            +"EMD Table component:"
-        }
-        // p { +(props.content ?: "HZ")  }
 
         if (props.content != null) {
             table {
-                //console.log(props.content)
-                val json = JSON.parse<Json>(props.content!!)
-                //console.log(json["events"])
-                val events = json["events"].unsafeCast<Array<Json>>()
-                events.forEach { event ->
+                caption {
+                    +"EMD Table component:"
+                }
+                thead {
                     tr {
-                        // console.log(event)
-                        val ref = event["reference"].unsafeCast<Json>()
-                        val event_number = ref["event_number"]
-                        val file_path = ref["file_path"]
-                        val storage_name = ref["storage_name"]
-                        val track_number = event["parameters"].unsafeCast<Json>()["track_number"]
-                        td { + event_number.toString() }
-                        td { + file_path.toString() }
-                        td { + storage_name.toString() }
-                        td { + track_number.toString() }
+                        th { +"Event number" }
+                        th { +"File path" }
+                        th { +"Storage name" }
+                        props.pageConfig.parameters.forEach { it ->
+                            th {
+                                +it.name
+                            }
+                        }
+                    }
+                }
+                tbody {
+                    //console.log(props.content)
+                    val json = JSON.parse<Json>(props.content!!)
+                    //console.log(json["events"])
+                    val events = json["events"].unsafeCast<Array<Json>>()
+                    events.forEach { event ->
+                        tr {
+                            // console.log(event)
+                            val ref = event["reference"].unsafeCast<Json>()
+                            val event_number = ref["event_number"]
+                            val file_path = ref["file_path"]
+                            val storage_name = ref["storage_name"]
+
+                            td { +event_number.toString() }
+                            td { +file_path.toString() }
+                            td { +storage_name.toString() }
+
+                            props.pageConfig.parameters.forEach { it ->
+                                val param_value = event["parameters"].unsafeCast<Json>()[it.name]
+                                td { +param_value.toString() }
+                            }
+
+                        }
                     }
                 }
             }
