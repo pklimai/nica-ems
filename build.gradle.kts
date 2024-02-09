43be63e6b7f863ed977f6fe6c55571a0145235f2
Peter Klimai <pklimai@gmail.com>
2024-02-09 15:19:03 +0300

    Bump Kotlin version and jvmTarget

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
-val kotlinVersion = "1.8.20"
+val kotlinVersion = "1.9.22"
 val serializationVersion = "1.5.0"
 val ktorVersion = "2.2.4"
 val logbackVersion = "1.4.6"
@@ -9,9 +9,9 @@ val reactVersion = "18.2.0-pre.537"
 val muiVersion = "5.10.0-pre.537"
 
 plugins {
-    kotlin("multiplatform") version "1.8.20"
+    kotlin("multiplatform") version "1.9.22"
     application //to run JVM part
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "ru.mipt.npm.nica.ems"
@@ -116,7 +116,7 @@ tasks.getByName<Jar>("jvmJar") {
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "15"
+            jvmTarget = "17"
         }
     }
 }
