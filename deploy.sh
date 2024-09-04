0991c03fbe0a5be64da39696a5f25de2f0af2bb0
Alexander Chebotov <lokzzzor@gmail.com>
2024-09-04 16:26:30 +0300

    update docker compose setup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-if [[ $(sudo docker ps -a -q --filter "name=nica-ems") ]]; then
-sudo docker stop $(sudo docker ps -a -q --filter "name=nica-ems") &>/dev/null; sudo docker rm $(sudo docker ps -a -q --filter "name=nica-ems") &>/dev/null
-echo '--- Containers have been cleared'
-fi
-sh gradlew installDist
-sudo docker-compose up --build -d
+sudo docker compose down -v
+sudo docker compose up --build -d
 
